19974dabcfcd32af16d9072e9368d3352bf76c8b
Misranfuadi <misranfalcon@gmail.com>
2019-07-03 18:47:07 +0700

    change bg-modal

--- resources/views/kelas/index.blade.php
+++ resources/views/kelas/index.blade.php
@@ -23,12 +23,12 @@
     <!-- Modal -->
     <div class="modal fade" id="modalForm" tabindex="-1" role="dialog" data-backdrop="static">
         <div class="modal-dialog" role="document">
-            <div class="modal-content" id="modal_content"></div>
+            <div class="modal-content bg-secondary" id="modal_content"></div>
         </div>
     </div>
     <div class="modal fade" id="modalDelete" tabindex="-1" role="dialog" data-backdrop="static">
         <div class="modal-dialog" role="document">
-            <div class="modal-content">
+            <div class="modal-content bg-secondary">
                 <div class="modal-header">
                     <h5 class="modal-title">Delete Confirmation</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -41,8 +41,8 @@
                     <input type="hidden" id="delete_id"/>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-danger"
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary"
                     onclick="ajaxDelete('{{ url('kelas/delete') }}/'+$('#delete_id').val(),$('#delete_token').val())">Delete</button>
                 </div>
             </div>
